3d302ece0dcb35589f92b597c0667556e7e05c74
JesusDavid2002 <jesusdavidsepu@gmail.com>
2025-04-24 15:04:30 +0200

    fix: Creacion de nuevo json para drive + control de inhabilitacion

--- src/main/java/com/ucam/flexicoche/service/GoogleDriveService.java
+++ src/main/java/com/ucam/flexicoche/service/GoogleDriveService.java
@@ -3,13 +3,14 @@ package com.ucam.flexicoche.service;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
@@ -18,15 +19,21 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
 
 @Service
 public class GoogleDriveService {
-    private static final String CREDENTIALS_FILE_PATH = "piis-flexicoche-304a9ebb3cba.json"; // debe estar en src/main/resources
+    //private static final String CREDENTIALS_FILE_PATH = "piis-flexicoche-304a9ebb3cba.json"; // debe estar en src/main/resources
     private static final String FOLDER_ID = "14KeGmKLUw68F7lcWNDn8IIOMCgxBURQD";
 
     private Drive getDriveService() throws IOException {
-        InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH);
+        //InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH);
+
+    	String CREDENTIALS_FILE_PATH = System.getenv("GOOGLE_CREDENTIALS_JSON");
+
+        //InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH);
+    	
+        InputStream credentialsStream = Files.newInputStream(Paths.get(CREDENTIALS_FILE_PATH));
+
         if (credentialsStream == null) {
             throw new FileNotFoundException("No se encontró el archivo de credenciales en resources.");
         }
